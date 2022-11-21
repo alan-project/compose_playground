491cf60448f0a509eab1a27dc6920d6d8f78eefc
AlanKim <banana205@gmail.com>
2022-11-21 18:14:27 -0500

    dynamic content concept

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -16,32 +17,50 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
+
+private val namesList: ArrayList<String> = arrayListOf("Kim", "Elin", "Joyel", "Cho")
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
-            MainScreen()
+            GreetingList(names = namesList)
         }
     }
 }
 
 @Composable
-fun MainScreen() {
-    Surface(
-        color = Color.DarkGray,
-        modifier = Modifier.fillMaxSize()
+fun GreetingList(names: List<String>) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.SpaceEvenly,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
+        for (name in names) {
+            Greeting(name)
+        }
 
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ){
+        Button(onClick = { namesList.add("New Name") }) {
+            Text("Add new name")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name")
+}
+
+
+@Composable
+fun MainScreen() {
+    Surface(color = Color.DarkGray, modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+
+            Row(modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly) {
                 ColoredSquare(Color.Blue)
                 ColoredSquare(Color.Cyan)
             }
@@ -54,12 +73,9 @@ fun MainScreen() {
 
 @Composable
 fun ColoredSquare(color: Color) {
-    Surface(
-        color = color,
-        modifier = Modifier
-            .height(100.dp)
-            .width(100.dp)
-    ) {}
+    Surface(color = color, modifier = Modifier
+        .height(100.dp)
+        .width(100.dp)) {}
 }
 
 @Composable
@@ -76,12 +92,16 @@ fun GreetingText(name: String) {
             fontWeight = FontWeight.SemiBold,
             fontSize = 18.sp)*/
         style = MaterialTheme.typography.h5,
-        fontWeight = FontWeight.SemiBold
-    )
+        fontWeight = FontWeight.SemiBold)
+}
+
+@Composable
+fun DefaultPreviewMainActivity() {
+    MainScreen()
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreviewMainActivity() {
-    MainScreen()
+fun GreetingList() {
+    GreetingList(names = namesList)
 }
