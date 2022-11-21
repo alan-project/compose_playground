347a106ecf2a4d6587c1dcfa066a801302b627f0
AlanKim <banana205@gmail.com>
2022-11-21 17:50:27 -0500

    Core UI elements

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -21,11 +21,45 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            //because both are composable
-            GreetingText("Android")
-//            Text("Hello Android")
+            MainScreen()
         }
     }
+}
+
+@Composable
+fun MainScreen() {
+    Surface(
+        color = Color.DarkGray,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ){
+                ColoredSquare(Color.Blue)
+                ColoredSquare(Color.Cyan)
+            }
+            ColoredSquare(Color.Magenta)
+            ColoredSquare(Color.White)
+            ColoredSquare(Color.Green)
+        }
+    }
+}
+
+@Composable
+fun ColoredSquare(color: Color) {
+    Surface(
+        color = color,
+        modifier = Modifier
+            .height(100.dp)
+            .width(100.dp)
+    ) {}
 }
 
 @Composable
@@ -48,15 +82,6 @@ fun GreetingText(name: String) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingButton() {
-    Button(onClick = { }) {
-        GreetingText(name = "button")
-        GreetingText(name = "remaining button")
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
 fun DefaultPreviewMainActivity() {
-    GreetingText("world")
+    MainScreen()
 }
