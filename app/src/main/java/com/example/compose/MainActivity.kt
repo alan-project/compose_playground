b3ebc8791dddb3b2240d486968924c8985597f57
AlanKim <banana205@gmail.com>
2022-11-21 22:42:48 -0500

    added viewModel

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -5,103 +5,64 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.MutableLiveData
 
-
-private val namesList: ArrayList<String> = arrayListOf("Kim", "Elin", "Joyel", "Cho")
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GreetingList(names = namesList)
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun GreetingList(names: List<String>) {
+fun MainScreen(viewModel: MainViewModel = MainViewModel()) {
+    val newNameStateContent = viewModel.textFieldState.observeAsState("")
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        for (name in names) {
-            Greeting(name)
-        }
+        GreetingMessage(
+            newNameStateContent.value
+        ) { newName -> viewModel.onTextChanged(newName) }
+    }
+}
 
-        Button(onClick = { namesList.add("New Name") }) {
-            Text("Add new name")
-        }
+@Composable
+fun GreetingMessage(
+    textFieldValue: String,
+    textFieldUpdate: (newName: String) -> Unit
+) {
+
+    TextField(value = textFieldValue, onValueChange = textFieldUpdate)
+    Button(onClick = { }) {
+        Text(textFieldValue)
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name")
-}
-
-
-@Composable
-fun MainScreen() {
-    Surface(color = Color.DarkGray, modifier = Modifier.fillMaxSize()) {
-        Column(modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.SpaceEvenly,
-            horizontalAlignment = Alignment.CenterHorizontally) {
-
-            Row(modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceEvenly) {
-                ColoredSquare(Color.Blue)
-                ColoredSquare(Color.Cyan)
-            }
-            ColoredSquare(Color.Magenta)
-            ColoredSquare(Color.White)
-            ColoredSquare(Color.Green)
-        }
-    }
-}
-
-@Composable
-fun ColoredSquare(color: Color) {
-    Surface(color = color, modifier = Modifier
-        .height(100.dp)
-        .width(100.dp)) {}
-}
-
-@Composable
-fun GreetingText(name: String) {
-    //built-in composable
-    Text(text = "Hello $name!",
-        modifier = Modifier
-            .clickable { }
-            .padding(all = 24.dp)
-            .width(240.dp)
-            .height(200.dp),
-/*        style = TextStyle(
-            color = Color.Blue,
-            fontWeight = FontWeight.SemiBold,
-            fontSize = 18.sp)*/
-        style = MaterialTheme.typography.h5,
-        fontWeight = FontWeight.SemiBold)
-}
-
-@Composable
-fun DefaultPreviewMainActivity() {
-    MainScreen()
+    Text(
+        text = "Hello $name!",
+        style = MaterialTheme.typography.h5
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingList() {
-    GreetingList(names = namesList)
+fun DefaultPreview() {
+    MainScreen()
 }
---
+++ app/src/main/java/com/example/compose/MainViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.compose
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    val textFieldState = MutableLiveData("")
+
+    fun onTextChanged(newText: String) {
+        textFieldState.value = newText
+    }
+}
